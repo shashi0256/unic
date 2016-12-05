57d5ff728182ef9ad9c7f38490b001289650550d
Olaf Otto <olaf@x100.de>
2016-12-05 09:24:47 +0100

    neba-158: Sample implementation to reproduce the use case described in https://github.com/unic/neba/issues/158
    - Added sampke for child -> parent reference
    - Change of commons-lang version for compatibility with vanilla AEM 6.2

--- src/main/java/to/adapt/neba/api/content/Image.java
+++ src/main/java/to/adapt/neba/api/content/Image.java
@@ -1,9 +1,14 @@
 package to.adapt.neba.api.content;
 
+import io.neba.api.annotations.Path;
 import io.neba.api.annotations.ResourceModel;
+import io.neba.api.resourcemodels.Optional;
 
 @ResourceModel(types = "neba-sample/components/image")
 public class Image {
+    @Path("..")
+    private Optional<ProductSummary> summary;
+
     private String alt;
 
     public String getAlt() {
